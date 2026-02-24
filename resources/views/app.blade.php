055f9aca70df04f9a9bd068663d4a18ab3c7fd7b
yha computer <yhacomputer@gmail.com>
2026-02-24 16:12:03 +0630

    fix all

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,6 +27,9 @@
         <!-- Vite Scripts -->
         @viteReactRefresh
         @vite(['resources/js/app.jsx', 'resources/css/app.css'])
+
+        <!-- Bootstrap JS for accordion and other components -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </head>
     <body>
         @inertia
