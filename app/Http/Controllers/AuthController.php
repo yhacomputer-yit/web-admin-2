8e128e2420b10f57d35f26b53d4655a4f50953f9
yha computer <yhacomputer@gmail.com>
2026-04-01 12:01:03 +0630

    fix all

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -63,7 +63,7 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('login');
+        return redirect()->route('loginPage');
     }
 
     // direct register page
